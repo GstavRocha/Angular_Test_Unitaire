330a762996bbe39bce972be9ef8f8ccab75cc877
GustavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2023-06-12 10:56:52 -0300

    analisando os testes

--- src/app/service/user.service.spec.ts
+++ src/app/service/user.service.spec.ts
@@ -2,15 +2,24 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 
-describe('UserService', () => {
-  let service: UserService;
+describe('UserService',()=>{
+  let userService: UserService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  beforeEach(()=>{
+    userService = new UserService();
+  })
+  it('Deve retornar um usuário existente por ID e por nome', ()=>{
+    const userId:number = 2;
+    const userName:string = "Maria"
+    const user = userService.getUserById(userId, userName);
+    expect(user).toBeDefined();
+    expect(user?.id).toEqual(userId);
+    expect(user?.name).toEqual(userName);
+  })
+  it('deve retornar undefined para um usuário inexistente por Id', ()=>{
+    const userId: number = 10;
+    const userName: string = "Paulo";
+    const user = userService.getUserById(userId,userName);
+    expect(user).toBeUndefined();
+  })
+})
